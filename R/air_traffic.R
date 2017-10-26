295d458d9ab299890cc46eb023cd662660831780
Mathew Ling <m.ling@deakin.edu.au>
2017-10-26 14:22:14 +1100

    model for cleaning

--- R/air_traffic.R
+++ R/air_traffic.R
@@ -1,6 +1,27 @@
 library(tidyverse)
 library(readxl)
 
+cleaner <- function(x){ 
+  #not functional
+  rename( 
+    airportyear = X__1,
+    airport = AIRPORT,
+    year = Year,
+    rank = Rank,
+    dom_inbound     = INBOUND,
+    dom_outbound    = OUTBOUND,
+    dom_total       = TOTAL,
+    int_inbound     = INBOUND__1,
+    int_outbound    = OUTBOUND__1,
+    int_total       = TOTAL__1,
+    total__inbound  = INBOUND__2,
+    total__outbound = OUTBOUND__2,
+    total__total    = TOTAL__2
+  ) %>% 
+    gather(airportyear, -c(airport, year, rank)) %>% 
+    select(-c(airportyear, rank)) 
+}
+
 download.file("https://bitre.gov.au/publications/ongoing/files/WebAirport_CY_1985-2016.xls",
               mode = "wb",
               destfile = "temp.xls")
@@ -20,6 +41,13 @@ international_freight_raw <- readxl::read_xls("temp.xls",
                                            skip = 6
 )
 
+
+
+aircraft_movements <-  aircraft_movements_raw %>% cleaner()
+
+airport_passengers <- airport_passengers_raw %>% cleaner()
+ 
+  
 # Rename columns 
 
 # TODO: tidy 
