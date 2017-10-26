de1496f93c5f901ae52918d6815d008ef04b04b4
Mathew Ling <m.ling@deakin.edu.au>
2017-10-27 09:49:38 +1100

    Adding package help file

---
+++ R/ozflights-package.R
@@ -0,0 +1,15 @@
+#' ozflights
+#'
+#' A package to obtain Australian airport traffic data between 1985-2016 from BITRE.
+#' Contains international freight data and both international and domestic data on number
+#' of passengers, and flight movements - for both regional and metropolitan airports.
+#'
+#' @name ozflights
+#' @docType package
+#' @import dplyr
+#' @import tidyr
+#' @import readxl
+#' @import magrittr
+#' @import utils
+#'
+NULL
